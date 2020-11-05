d71136634e8f9caac859b2d6f2563cdfae9239a6
Ben Rowe <brow495@gmail.com>
2020-11-05 08:16:14 -0700

    Add Fauci image.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './style.css'
+import * as fauci from '../public/assets/fauci-advisory-council.jpg'
 import Chat from './components/chat'
 
 export default function App() {
   return (
     <div className="App">
-      <header style={{paddingTop: '4rem', paddingBottom: '2rem'}}>NurseBot</header>
+      <header style={{paddingTop: '4rem', paddingBottom: '2rem'}}>
+        NurseBot
+        <img src={fauci} alt='Fauci' />
+      </header>
       <Chat />
     </div>
   );
