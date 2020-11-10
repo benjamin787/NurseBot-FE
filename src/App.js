3b9119718cdf779d435680c119fb9cc0b37c7293
Ben Rowe <brow495@gmail.com>
2020-11-10 13:43:28 -0700

    Add popover from material ui.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import './style.css'
 import fauci from './assets/fauci-advisory-council.jpg'
 import Chat from './components/chat'
+import SimplePopover from './components/simplepopover'
 
 export default function App() {
   return (
     <div className="App">
-      <header style={{paddingTop: '3rem', paddingBottom: '2rem', paddingLeft: '5rem', display: 'flex'}}>
+      <header style={{paddingTop: '3rem', paddingBottom: '2rem', paddingLeft: '5rem'}}>
         <img className='Fauci' src={fauci} alt='Fauci' />
         <h1>NurseBot</h1>
       </header>
-      <Chat />
+      <body>
+        <span className='popover-container'>
+          <SimplePopover />
+        </span>
+        <Chat />
+      </body>
       <footer></footer>
     </div>
   );
