7a4f4b08adb554758605a38565a01af79ddf36b5
Ben Rowe <brow495@gmail.com>
2021-01-04 23:24:48 -0700

    Add AttachFileIcon.

--- src/components/clickportal.js
+++ src/components/clickportal.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import { ClickAwayListener } from '@material-ui/core'
 import MenuBookIcon from '@material-ui/icons/MenuBook'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
 
 export default function ClickPortal() {
     const [open, setOpen] = useState(false) 
+    const [open2, setOpen2] = useState(false) 
 
     const handleClick = () => {
         setOpen(!open)
+    }
+    const handleClick2 = () => {
+        setOpen2(!open2)
     }
 
     const handleClickAway = () => {
@@ -14,17 +19,31 @@ export default function ClickPortal() {
     }
 
     return (
-        <ClickAwayListener onClickAway={handleClickAway} >
-            <div>
-                <MenuBookIcon type='button' className='icon' fontSize='large' onClick={handleClick} >
-                    popover here
-                </MenuBookIcon>
-                {open ? (
-                    <span>
-                        Directions to closest test site.
-                    </span>
-                ) : null}
-            </div>
-        </ClickAwayListener>
+        <div>
+            <ClickAwayListener onClickAway={handleClickAway} >
+                <div>
+                    <MenuBookIcon type='button' className='icon' fontSize='large' onClick={handleClick} >
+                        popover here
+                    </MenuBookIcon>
+                    {open ? (
+                        <span>
+                            Directions to closest test site.
+                        </span>
+                    ) : null}
+                </div>
+            </ClickAwayListener>
+            <ClickAwayListener onClickAway={handleClickAway} >
+                <div>
+                    <AttachFileIcon type='button' className='icon' fontSize='large' onClick={handleClick2} >
+                        popover here
+                    </AttachFileIcon>
+                    {open2 ? (
+                        <span>
+                            Directions to closest test site.
+                        </span>
+                    ) : null}
+                </div>
+            </ClickAwayListener>
+        </div>
     )
 }
